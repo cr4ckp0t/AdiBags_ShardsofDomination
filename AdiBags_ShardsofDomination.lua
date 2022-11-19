ead463cc87e558b283644e9c6977a5747588f8c6
Fentara <118627796+Fentara@users.noreply.github.com>
2022-11-19 11:32:31 -0700

    Update AdiBags_ShardsofDomination.lua

--- AdiBags_ShardsofDomination.lua
+++ AdiBags_ShardsofDomination.lua
@@ -109,7 +109,7 @@ function shardFilter:Filter(slotData)
     if slotData.bag == BANK_CONTAINER then
         tooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(slotData.slot, nil))
     else
-        tooltip:SetBagItem(slotData.bag, slotData.slot)
+        GameTooltip:SetBagItem(slotData.bag, slotData.slot)
     end
 
     tooltip:Hide()
